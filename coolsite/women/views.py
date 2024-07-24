230f60635fc9c37243f343f5b566ab97f2608e4c
alexandr_fin_calc <aleksandraseev14212@gmail.com>
2024-07-24 11:54:07 -0700

    8 video

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -4,11 +4,20 @@ from django.shortcuts import render, redirect
 from .models import  *
 
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [
+    {'title': "О сайте", 'url_name': 'about'},
+    {'title': "Добавить статью", 'url_name': 'addpage'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
+    {'title': "Войти", 'url_name': 'login'}
+]
 
 def index(requests):
     posts = Women.objects.all()
-    return render(requests, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {'posts': posts,
+               'menu': menu,
+               'title': 'Главная страница'
+               }
+    return render(requests, 'women/index.html', context=context)
 
 
 def about(requests):
@@ -19,18 +28,21 @@ def about(requests):
 #     return HttpResponse('страница приложения women.')
 
 
-def categories(requests, catid):
-    if(requests.GET):
-        print(requests.GET)
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
+def addpage(requests):
+    return HttpResponse('Добавить статью')
 
 
-def archive(requests, year):
-    if int(year) > 2020:
-        return redirect('home', permanent= True)
+def contact(requests):
+    return HttpResponse('Обратная связь')
 
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def login(requests):
+    return HttpResponse('Авторизация')
 
 
 def pageNotFound(requests, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
+
+def show_post(requests, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
