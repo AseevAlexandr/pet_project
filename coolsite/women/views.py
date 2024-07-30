5ae3fb1ba281d03a494e8b96bc5ad8554133e73c
alexandr_fin_calc <aleksandraseev14212@gmail.com>
2024-07-30 11:03:31 -0700

    17 video

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -29,7 +30,12 @@ class WomenHome(DataMixin, ListView):
 
 
 def about(requests):
-    return render(requests, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+    contact_list = Women.objects.all()
+    paginator = Paginator(contact_list,3)
+
+    page_number = requests.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(requests, 'women/about.html', {'page_obj': page_obj, 'menu': menu, 'title': 'О сайте'})
 
 
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
