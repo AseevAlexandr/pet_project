d40936ae2d407d9dc24316e2b9be36ab1b2358b8
alexandr_fin_calc <aleksandraseev14212@gmail.com>
2024-07-31 13:21:18 -0700

    19,20 video

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -2,12 +2,18 @@ from django.urls import path, re_path
 
 from .views import *
 
+
+
+
+
 urlpatterns = [
     path('', WomenHome.as_view(), name= 'home'),
     path('about/', about, name= 'about'),
     path('addpage', AddPage.as_view(), name='addpage'),
     path('contact', contact, name='contact'),
-    path('login', login, name='login'),
+    path('login', LoginUser.as_view(), name='login'),
+    path('logout', LogoutUser, name='logout'),
+    path('register', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category')
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
@@ -56,8 +58,8 @@ def contact(requests):
     return HttpResponse('Обратная связь')
 
 
-def login(requests):
-    return HttpResponse('Авторизация')
+# def login(requests):
+#     return HttpResponse('Авторизация')
 
 
 def pageNotFound(requests, exception):
@@ -95,3 +97,39 @@ class WomenCategory(DataMixin, ListView):
         return dict(list(context.items()) + list(c_def.items()))
 
 
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.requests, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def LogoutUser(requests):
+    logout(requests)
+    return redirect('login')
