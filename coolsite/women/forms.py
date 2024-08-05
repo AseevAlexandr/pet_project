5292891ab943c1ad84bff86cd91400201af5d037
alexandr_fin_calc <aleksandraseev14212@gmail.com>
2024-08-05 08:09:11 -0700

    final version

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -34,10 +34,9 @@ class RegisterUserForm(UserCreationForm):
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
-
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class LoginUserForm(AuthenticationForm):
